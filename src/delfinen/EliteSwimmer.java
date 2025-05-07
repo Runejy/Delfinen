69044bb7820aa5f2030e61a137a4d24cf35a2e45
August <augustgosden@gmail.com>
2025-05-07 12:02:15 +0200

    final skabelon

--- src/delfinen/EliteSwimmer.java
+++ src/delfinen/EliteSwimmer.java
@@ -1,8 +1,25 @@
 package delfinen;
 
 public class EliteSwimmer extends Member{
+    private String team;
+
+    private enum discipline{
+
+    }
 
     EliteSwimmer(String name, int age, String mail){
         super(name, age, mail);
+    }
+
+    public void addTrainer(){
+
+    }
+
+    public void addDiscipline(){
+
+    }
+
+    public void updateProfile(){
+
     }
 }
--- src/delfinen/SwimmingClub.java
+++ src/delfinen/SwimmingClub.java
@@ -2,4 +2,24 @@ package delfinen;
 
 public class SwimmingClub {
 
+
+    public void listOfMembers(){
+
+    }
+
+    public void totalMembers(){
+
+    }
+
+    public void totalMembersByMemberType(){
+
+    }
+
+    public void listOfTrainers(){
+
+    }
+
+    public void top5Swimmers(){
+
+    }
 }
